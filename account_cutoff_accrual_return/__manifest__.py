2d2aa0d8d63b42903844088ce0f030a71d2cb08a
Jacques-Etienne Baudoux <je@bcim.be>
2018-03-06 13:56:01 +0100

    [ADD] account_cutoff_accrual_return

---
+++ account_cutoff_accrual_return/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Accrual Return Picking',
+    'version': '10.0.0.1.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Accrued Expense & Accrued Revenue from Return Pickings',
+    'depends': ['account_cutoff_accrual_base', 'stock'],
+    'data': [
+        'views/account_cutoff.xml',
+        'views/company.xml',
+        'views/stock_location.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
