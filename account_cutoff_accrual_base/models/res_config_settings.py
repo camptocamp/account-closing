b4f6c94636d103edbd17be7498e8eafaaadb8ede
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-11-30 09:06:48 +0100

    [11.0][IMP] Move options to accounting settings
    Up to now the configuration options were displayed on the company view.
    Here they are moved to the accounting settings.

---
+++ account_cutoff_accrual_base/models/res_config_settings.py
@@ -0,0 +1,51 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    default_accrued_revenue_account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Default Account for Accrued Revenues',
+        domain=[('deprecated', '=', False)],
+    )
+
+    default_accrued_expense_account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Default Account for Accrued Expenses',
+        domain=[('deprecated', '=', False)],
+    )
+
+    default_accrual_revenue_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Default Journal for Accrued Revenues'
+    )
+
+    default_accrual_expense_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Default Journal for Accrued Expenses'
+    )
+
+    @api.multi
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        company = self.env.user.company_id
+        res.update({
+            'default_accrued_revenue_account_id': company.default_accrued_revenue_account_id.id,
+            'default_accrued_expense_account_id': company.default_accrued_expense_account_id.id,
+            'default_accrual_revenue_journal_id': company.default_accrual_revenue_journal_id.id,
+            'default_accrual_expense_journal_id': company.default_accrual_expense_journal_id.id,
+        })
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        company = self.env.user.company_id
+        company.default_accrued_expense_account_id = self.default_accrued_expense_account_id
+        company.default_accrued_revenue_account_id = self.default_accrued_revenue_account_id
+        company.default_accrual_expense_journal_id = self.default_accrual_expense_journal_id
+        company.default_accrual_revenue_journal_id = self.default_accrual_revenue_journal_id
