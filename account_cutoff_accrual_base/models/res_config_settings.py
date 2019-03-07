18ff9902c36936c3126122684404d22db38fa458
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-03-07 14:48:42 +0100

    fixup! [11.0][IMP] Move options to accounting settings

--- account_cutoff_accrual_base/models/res_config_settings.py
+++ account_cutoff_accrual_base/models/res_config_settings.py
@@ -8,48 +8,21 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     default_accrued_revenue_account_id = fields.Many2one(
-        comodel_name='account.account',
+        related='company_id.default_accrued_revenue_account_id',
         string='Default Account for Accrued Revenues',
-        domain=[('deprecated', '=', False)],
     )
 
     default_accrued_expense_account_id = fields.Many2one(
-        comodel_name='account.account',
+        related='company_id.default_accrued_expense_account_id',
         string='Default Account for Accrued Expenses',
-        domain=[('deprecated', '=', False)],
     )
 
     default_accrual_revenue_journal_id = fields.Many2one(
-        comodel_name='account.journal',
+        related='company_id.default_accrual_revenue_journal_id',
         string='Default Journal for Accrued Revenues'
     )
 
     default_accrual_expense_journal_id = fields.Many2one(
-        comodel_name='account.journal',
+        related='company_id.default_accrual_expense_journal_id',
         string='Default Journal for Accrued Expenses'
     )
-
-    config_fields = [
-        'default_accrued_revenue_account_id',
-        'default_accrued_expense_account_id',
-        'default_accrual_revenue_journal_id',
-        'default_accrual_expense_journal_id',
-    ]
-
-    @api.multi
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        company = self.env.user.company_id
-        values = dict(
-            [(field, company[field].id) for field in self.config_fields]
-        )
-        res.update(values)
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        values = dict(
-            [(field, self[field]) for field in self.config_fields]
-        )
-        self.env.user.company_id.update(values)
--- account_cutoff_base/models/res_config_settings.py
+++ account_cutoff_base/models/res_config_settings.py
@@ -8,21 +8,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     default_cutoff_journal_id = fields.Many2one(
-        'account.journal',
+        related='company_id.default_cutoff_journal_id',
         string='Default Cut-off Journal'
     )
-
-    @api.multi
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        company = self.env.user.company_id
-        res.update({
-            'default_cutoff_journal_id': company.default_cutoff_journal_id.id
-        })
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        company = self.env.user.company_id
-        company.default_cutoff_journal_id = self.default_cutoff_journal_id
